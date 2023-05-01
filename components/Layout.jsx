8a3707c9cf8df34b17639119ae9ffff036db6500
Cuboost <121436798+Cuboost@users.noreply.github.com>
2023-04-30 18:33:29 -0700

    Updated Metadata

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,26 @@
+import Head from "next/head";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
 
     return (
-        <div>
-            <div className=" m-auto w-10/12 md:w-9/12 lg:w-8/12">
+        <>
+            <Head>
+                <title>Cuboost Portfolio</title>
+                <meta name="description" content="Cuboost Portfolio, learn about the developper..." />
+                <meta name="author" content="Cuboost" />
+                {/* Color */}
+                <meta name="theme-color" content="#38b2ac"></meta>
+                {/* Favicon */}
+                <link rel="icon" href="/images/icons/favicon.ico" />
+            </Head>
+            <main className=" m-auto w-10/12 md:w-9/12 lg:w-8/12">
                 <Navbar />
                 {children}
                 <Footer />
-            </div>
-        </div>
+            </main>
+        </>
     );
 };
 
